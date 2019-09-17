1309ae9f4fe8ff372af56e8f6cdf0ab647e6e956
julkku <julius@uusinarkaus.com>
2019-09-17 11:33:11 +0300

    changed task deadline from current day to year ago

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -3,7 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, validators, DateField, Form
 from wtforms.fields.html5 import DateField
 from wtforms_components import DateRange
+from dateutil.relativedelta import relativedelta
 
+def year_ago():
+    date = datetime.today() - relativedelta(years=1)
+    return date.date
 
 class TaskForm(FlaskForm):
     name = StringField("Task name", [validators.Length(min=2)], render_kw={"placeholder": "Task name"})
@@ -13,8 +17,10 @@ class TaskForm(FlaskForm):
         format='%Y-%m-%d',
 
         default=datetime.today,
-        validators=[DateRange(min=date.today())]
+        validators=[DateRange(min=(year_ago()))]
     )
 
     class Meta:
         csrf = False
+
+
