654ff8dbbea0745ffc0244d163e7d7e4912423d4
julkku <julius@uusinarkaus.com>
2019-09-26 10:44:06 +0300

    Switched dev to postgres

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 import os
 
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+    # engine = create_engine('postgres://postgres:password@localhost:5432/tasks')
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres:newPassword@localhost:5432/postgres"
     app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
